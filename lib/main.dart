ca327db589d16313f8d72d77cb433a524dfc92d8
Bwire Phabiano <bwirephabiano939@gmail.com>
2019-09-16 18:26:06 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main ()  => runApp(
+
+    Sring name; Michrophone
+
+    MaterialApp(
+      home: Text('Hello World')
+    )
+);
+
+
+
+/*
+MyApp extends StatelessWidget{
+  //This widget is the root of your application.
+  @override
+  Widget build ( BuildContext context)[
+    return MaterialApp(
+      title:'Flutter Demo',
+  theme:ThemeData(
+      primarySwatch: Colors.black,
+      ),//ThemeData
+  home:MyHomePage(title:'Flutter Demo Home Page'),
+      );// MaterialApp
+  ]
+  }
+}
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+}
+*/
